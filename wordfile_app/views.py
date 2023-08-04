9ce68257f30967ca4c854f4921892a4782fbf039
git2chirag <ramnanichirag70@gmail.com>
2023-08-04 13:06:25 +0530

    vercel config 2

--- wordfile_app/views.py
+++ wordfile_app/views.py
@@ -7,24 +7,24 @@ import os
 
 class WordFileView(APIView):
     def post(self, request, format=None):
-        # Get JSON data from request
+        
         data = json.loads(request.body)
 
-        # Create a Document
+        
         doc = Document()
 
-        # Add data to Document
+        
         for key, value in data.items():
             doc.add_paragraph(f"{key}: {value}")
 
-        # Ensure 'temp' directory exists
+        
         os.makedirs('temp', exist_ok=True)
 
-        # Save Document
+        
         file_path = os.path.join('temp', 'output.docx')
         doc.save(file_path)
 
-        # Append data to Excel file
+        
         try:
             df_new = pd.DataFrame([data])
             file_name = 'data.xlsx'
@@ -35,7 +35,7 @@ class WordFileView(APIView):
         except Exception as e:
             return HttpResponse(f"Failed to append data to Excel file: {str(e)}")
 
-        # Return file as response
+       
         try:
             response = FileResponse(open(file_path, 'rb'), content_type='application/msword')
             response['Content-Disposition'] = f'attachment; filename="output.docx"'
--- wordfile_project/settings.py
+++ wordfile_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ts63^!o&d*l406)k0(a!06f#d1p&8$ro1+u+oax92#=zxccn@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
