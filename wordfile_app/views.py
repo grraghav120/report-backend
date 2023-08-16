216f2b8a086e1761345ed05bdd6a7aa24fbe172c
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-08-16 12:32:08 +0530

    pdf generate by raghav

--- wordfile_app/views.py
+++ wordfile_app/views.py
@@ -4,41 +4,57 @@ from docx import Document
 import pandas as pd
 import json
 import os
+# for pdf
+from io import BytesIO
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html  = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)#, link_callback=fetch_resources)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
+def data_collect_in_excel_file(data):
+    df_new = pd.DataFrame([data])
+    file_name = 'data.xlsx'
+    if os.path.isfile(file_name):
+        df_old = pd.read_excel(file_name)
+        df_new = pd.concat([df_old, df_new])
+    df_new.to_excel(file_name, index=False)
+
 
 class WordFileView(APIView):
     def post(self, request, format=None):
-        
         data = json.loads(request.body)
+        # print(data)
+        pdf = render_to_pdf('wordfile_app/report.html', data)
+        if pdf:
+            # Set response headers for download
+            response = HttpResponse(pdf, content_type='application/pdf')
+            response['Content-Disposition'] = 'inline; filename="Report.pdf"'
+            data_collect_in_excel_file(data)
+            return response
+        else:
+            return HttpResponse("Error generating PDF", status=500)
 
-        
-        doc = Document()
-
-        
-        for key, value in data.items():
-            doc.add_paragraph(f"{key}: {value}")
-
-        
-        os.makedirs('temp', exist_ok=True)
-
-        
-        file_path = os.path.join('temp', 'output.docx')
-        doc.save(file_path)
-
-        
-        try:
-            df_new = pd.DataFrame([data])
-            file_name = 'data.xlsx'
-            if os.path.isfile(file_name):
-                df_old = pd.read_excel(file_name)
-                df_new = pd.concat([df_old, df_new])
-            df_new.to_excel(file_name, index=False)
-        except Exception as e:
-            return HttpResponse(f"Failed to append data to Excel file: {str(e)}")
+        # try:
+        #     df_new = pd.DataFrame([data])
+        #     file_name = 'data.xlsx'
+        #     if os.path.isfile(file_name):
+        #         df_old = pd.read_excel(file_name)
+        #         df_new = pd.concat([df_old, df_new])
+        #     df_new.to_excel(file_name, index=False)
+        # except Exception as e:
+        #     return HttpResponse(f"Failed to append data to Excel file: {str(e)}")
 
        
-        try:
-            response = FileResponse(open(file_path, 'rb'), content_type='application/msword')
-            response['Content-Disposition'] = f'attachment; filename="output.docx"'
-            return response
-        except Exception as e:
-            return HttpResponse(f"Failed to return file: {str(e)}")
+        # try:
+        #     response = FileResponse(open(file_path, 'rb'), content_type='application/msword')
+        #     response['Content-Disposition'] = f'attachment; filename="output.docx"'
+        #     return response
+        # except Exception as e:
+        #     return HttpResponse(f"Failed to return file: {str(e)}")
--- wordfile_project/settings.py
+++ wordfile_project/settings.py
@@ -45,13 +45,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'wordfile_project.urls'
@@ -126,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS =[
+    "http://localhost:4200",  # Your Angular app's URL
+    # Add more allowed origins as needed
+]
