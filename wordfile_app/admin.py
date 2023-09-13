beaf263529c9d16e8137fde8561741795040db17
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-09-13 12:51:35 +0530

    admin medical

--- wordfile_app/admin.py
+++ wordfile_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MedicalData
 
-# Register your models here.
+class MedicalDataAdmin(admin.ModelAdmin):
+    @staticmethod
+    def get_all_fields(obj):
+        return [field.name for field in obj._meta.get_fields()]
+
+    list_display = get_all_fields(MedicalData)  # Display all fields in the admin list view
+admin.site.register(MedicalData, MedicalDataAdmin)
